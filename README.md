04a5f81555d3a74ae244d17bdeabf3ebb1f5ff24
vikkis <baheryan.vika@gmail.com>
2017-12-13 10:47:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+express http server
+що віддає статичні файли
+dockerfile
+
+research
+nginx proxy server( localhost:3033 має відкривати вміст нашого сервера )
---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+// const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  // res.send('hello there! how it`s going?');
+  res.sendFile(path.join(__dirname + '/volumes/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});

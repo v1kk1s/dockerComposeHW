3e6d46594ddeb5b0a51d07ae3ba37bc8849a4c7a
vikkis <baheryan.vika@gmail.com>
2017-12-19 18:05:27 +0200

    Add second server

--- src/server.js
+++ src/server.js
@@ -3,14 +3,14 @@ const path = require('path');
 // const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   // res.send('hello there! how it`s going?');
-  res.sendFile(path.join(__dirname + '/volumes/app.html'));
+  res.sendFile(path.join(__dirname + '/volumes/server.html'));
 });
 
 app.listen(port, () => {

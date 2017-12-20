dd27fbe426973964bac5c5c329a5dc3c07297f0f
vikkis <baheryan.vika@gmail.com>
2017-12-20 13:00:28 +0200

    Add request lib

--- src/app.js
+++ src/app.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const path = require('path');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const band = {
+  name: 'Make it snow',
+  author: 'Frank Sinatra',
+};
 
 app.get('/', (req, res) => {
-  // res.send('hello there! how it`s going?');
   res.sendFile(path.join(__dirname + '/volumes/app.html'));
 });
 
+app.get('/get', (req, res) => {
+  const { name } = req.query;
+  res.send(band[name]);
+
+  console.log('send name: ', band[name], req.query);
+});
+
+app.post('/set', (req, res) => {
+  const keys = Object.keys(req.body);
+
+  keys.map(key => (band[key] = req.body[key]));
+  res.send(band);
+
+  console.log('Request: ', req.body);
+});
+
 app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+  console.log(`App running on port ${port}`);
 });
--- src/server.js
+++ src/server.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const path = require('path');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('hello there! how it`s going?');
-  // res.sendFile(path.join(__dirname + '/volumes/server.html'));
+  res.sendFile(path.join(__dirname + '/volumes/server.html'));
+});
+
+app.get('/get-from-object/:name', (req, res) => {
+  res.send(req.params.name);
+  request.get(`http://app:3001/get/?name=${req.params.name}`);
+});
+
+app.post('/set-in-object', (req, res) => {
+  res.send(req.body);
+  request({
+    method: 'POST',
+    uri: 'http://app:3001/set',
+    multipart: [
+      {
+        'content-type': 'application/json',
+        body: JSON.stringify(req.body),
+      },
+    ],
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
